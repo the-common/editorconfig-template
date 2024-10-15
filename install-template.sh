05573c39257682fbd7785fbdd278254ab4242cf1
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2024-10-16 02:36:42 +0800

    fix(install-template): Fix non-compliant shell script indentation style
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- install-template.sh
+++ install-template.sh
@@ -14,58 +14,58 @@ set -o pipefail
 
 ## Runtime Dependencies Checking
 declare\
-	runtime_dependency_checking_result=still-pass\
-	required_software
+    runtime_dependency_checking_result=still-pass\
+    required_software
 
 for required_command in \
-	basename\
-	dirname\
-	install\
-	realpath\
-	rm; do
-	if ! command -v "${required_command}" &>/dev/null; then
-		runtime_dependency_checking_result=fail
+    basename\
+    dirname\
+    install\
+    realpath\
+    rm; do
+    if ! command -v "${required_command}" &>/dev/null; then
+        runtime_dependency_checking_result=fail
 
-		case "${required_command}" in
-			basename\
-			|dirname\
-			|install\
-			|realpath\
-			|rm)
-				required_software='GNU Coreutils'
-				;;
-			*)
-				required_software="${required_command}"
-				;;
-		esac
+        case "${required_command}" in
+            basename\
+            |dirname\
+            |install\
+            |realpath\
+            |rm)
+                required_software='GNU Coreutils'
+                ;;
+            *)
+                required_software="${required_command}"
+                ;;
+        esac
 
-		printf --\
-			'Error: This program requires "%s" to be installed and its executables in the executable searching paths.\n' \
-			"${required_software}" 1>&2
-		unset required_software
-	fi
+        printf --\
+            'Error: This program requires "%s" to be installed and its executables in the executable searching paths.\n' \
+            "${required_software}" 1>&2
+        unset required_software
+    fi
 done; unset required_command required_software
 
 if [ "${runtime_dependency_checking_result}" = fail ]; then
-	printf --\
-		'Error: Runtime dependency checking fail, the progrom cannot continue.\n' 1>&2
-	exit 1
+    printf --\
+        'Error: Runtime dependency checking fail, the progrom cannot continue.\n' 1>&2
+    exit 1
 fi; unset runtime_dependency_checking_result
 
 ## Non-overridable Primitive Variables
 ## BASHDOC: Shell Variables » Bash Variables
 ## BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters
 if [ -v 'BASH_SOURCE[0]' ]; then
-	RUNTIME_EXECUTABLE_PATH="$(realpath --strip "${BASH_SOURCE[0]}")"
-	RUNTIME_EXECUTABLE_FILENAME="$(basename "${RUNTIME_EXECUTABLE_PATH}")"
-	RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
-	RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "${RUNTIME_EXECUTABLE_PATH}")"
-	RUNTIME_COMMANDLINE_BASECOMMAND="${0}"
-	declare -r\
-		RUNTIME_EXECUTABLE_FILENAME\
-		RUNTIME_EXECUTABLE_DIRECTORY\
-		RUNTIME_EXECUTABLE_PATHABSOLUTE\
-		RUNTIME_COMMANDLINE_BASECOMMAND
+    RUNTIME_EXECUTABLE_PATH="$(realpath --strip "${BASH_SOURCE[0]}")"
+    RUNTIME_EXECUTABLE_FILENAME="$(basename "${RUNTIME_EXECUTABLE_PATH}")"
+    RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
+    RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "${RUNTIME_EXECUTABLE_PATH}")"
+    RUNTIME_COMMANDLINE_BASECOMMAND="${0}"
+    declare -r\
+        RUNTIME_EXECUTABLE_FILENAME\
+        RUNTIME_EXECUTABLE_DIRECTORY\
+        RUNTIME_EXECUTABLE_PATHABSOLUTE\
+        RUNTIME_COMMANDLINE_BASECOMMAND
 fi
 declare -ar RUNTIME_COMMANDLINE_ARGUMENTS=("${@}")
 
@@ -73,236 +73,236 @@ declare -ar RUNTIME_COMMANDLINE_ARGUMENTS=("${@}")
 ## This function is called near the end of the file,
 ## with the script's command-line parameters as arguments
 init(){
-	local flag_uninstall=false
-	local install_directory_xdg
+    local flag_uninstall=false
+    local install_directory_xdg
 
-	if ! process_commandline_arguments\
-			flag_uninstall; then
-		printf -- \
-			'Error: %s: Invalid command-line parameters.\n' \
-			"${FUNCNAME[0]}" \
-			1>&2
-		print_help
-		exit 1
-	fi
+    if ! process_commandline_arguments\
+            flag_uninstall; then
+        printf -- \
+            'Error: %s: Invalid command-line parameters.\n' \
+            "${FUNCNAME[0]}" \
+            1>&2
+        print_help
+        exit 1
+    fi
 
-	if ! determine_install_directory\
-			install_directory_xdg; then
-		printf -- \
-			'Error: Unable to determine install directory, installer cannot continue.\n' \
-			1>&2
-		exit 1
-	else
-		printf -- \
-			'Will be installed to: %s\n' \
-			"${install_directory_xdg}"
-		printf '\n'
-	fi
+    if ! determine_install_directory\
+            install_directory_xdg; then
+        printf -- \
+            'Error: Unable to determine install directory, installer cannot continue.\n' \
+            1>&2
+        exit 1
+    else
+        printf -- \
+            'Will be installed to: %s\n' \
+            "${install_directory_xdg}"
+        printf '\n'
+    fi
 
-	remove_old_installation\
-		"${install_directory_xdg}"
-	if [ "${flag_uninstall}" = true ]; then
-		printf -- \
-				'Software uninstalled successfully.\n'
-		exit 0
-	fi
+    remove_old_installation\
+        "${install_directory_xdg}"
+    if [ "${flag_uninstall}" = true ]; then
+        printf -- \
+                'Software uninstalled successfully.\n'
+        exit 0
+    fi
 
-	printf -- \
-		'Installing template files...\n'
-	mkdir \
-		--parents \
-		"${XDG_TEMPLATES_DIR}"
-	install \
-		--verbose \
-		--mode=u=rw,go=r \
-		"${RUNTIME_EXECUTABLE_DIRECTORY}/.editorconfig" \
-		"${install_directory_xdg}/EditorConfig Template.editorconfig"
-	printf '\n' # Seperate output from different operations
+    printf -- \
+        'Installing template files...\n'
+    mkdir \
+        --parents \
+        "${XDG_TEMPLATES_DIR}"
+    install \
+        --verbose \
+        --mode=u=rw,go=r \
+        "${RUNTIME_EXECUTABLE_DIRECTORY}/.editorconfig" \
+        "${install_directory_xdg}/EditorConfig Template.editorconfig"
+    printf '\n' # Seperate output from different operations
 
-	while true; do
-		printf -- \
-			'Do you want to install files to enable KDE support(y/N)?'
-		read -r answer
+    while true; do
+        printf -- \
+            'Do you want to install files to enable KDE support(y/N)?'
+        read -r answer
 
-		if [ -z "${answer}" ]; then
-			break
-		else
-			# lowercasewize
-			answer="$(
-				printf -- \
-					'%s' \
-					"${answer}" \
-					| tr '[:upper:]' '[:lower:]'
-			)"
+        if [ -z "${answer}" ]; then
+            break
+        else
+            # lowercasewize
+            answer="$(
+                printf -- \
+                    '%s' \
+                    "${answer}" \
+                    | tr '[:upper:]' '[:lower:]'
+            )"
 
-			if [ "${answer}" != n ] && [ "${answer}" != y ]; then
-				# wrong format, re-ask
-				continue
-			elif [ "${answer}" == n ]; then
-				break
-			else
-				printf 'Configuring templates for KDE...\n'
-				mkdir \
-					--parents \
-					"${HOME}/.local/share/templates"
-				install \
-					--verbose \
-					--mode=u=rw,go=r \
-					"${RUNTIME_EXECUTABLE_DIRECTORY}/.editorconfig" \
-					"${HOME}/.local/share/templates/EditorConfig Template.editorconfig"
-				install \
-					--verbose \
-					--mode=u=rw,go=r \
-					"${RUNTIME_EXECUTABLE_DIRECTORY}/Template Setup for KDE"/*.desktop \
-					"${HOME}/.local/share/templates"
-				break
-			fi
-		fi
-	done; unset answer
+            if [ "${answer}" != n ] && [ "${answer}" != y ]; then
+                # wrong format, re-ask
+                continue
+            elif [ "${answer}" == n ]; then
+                break
+            else
+                printf 'Configuring templates for KDE...\n'
+                mkdir \
+                    --parents \
+                    "${HOME}/.local/share/templates"
+                install \
+                    --verbose \
+                    --mode=u=rw,go=r \
+                    "${RUNTIME_EXECUTABLE_DIRECTORY}/.editorconfig" \
+                    "${HOME}/.local/share/templates/EditorConfig Template.editorconfig"
+                install \
+                    --verbose \
+                    --mode=u=rw,go=r \
+                    "${RUNTIME_EXECUTABLE_DIRECTORY}/Template Setup for KDE"/*.desktop \
+                    "${HOME}/.local/share/templates"
+                break
+            fi
+        fi
+    done; unset answer
 
-	printf 'Installation completed.\n'
+    printf 'Installation completed.\n'
 
 
-	exit 0
+    exit 0
 }; declare -fr init
 
 print_help(){
-	printf '# %s #\n' "${RUNTIME_EXECUTABLE_NAME}"
-	printf 'This program installs the templates into the system to make it accessible.\n\n'
+    printf '# %s #\n' "${RUNTIME_EXECUTABLE_NAME}"
+    printf 'This program installs the templates into the system to make it accessible.\n\n'
 
-	printf '## Command-line Options ##\n'
-	printf '### --help / -h ###\n'
-	printf 'Print this message\n\n'
+    printf '## Command-line Options ##\n'
+    printf '### --help / -h ###\n'
+    printf 'Print this message\n\n'
 
-	printf '### --uninstall / -u ###\n'
-	printf 'Instead of installing, attempt to remove previously installed product\n\n'
+    printf '### --uninstall / -u ###\n'
+    printf 'Instead of installing, attempt to remove previously installed product\n\n'
 
-	printf '### --debug / -d ###\n'
-	printf 'Enable debug mode\n\n'
+    printf '### --debug / -d ###\n'
+    printf 'Enable debug mode\n\n'
 
-	return 0
+    return 0
 }; declare -fr print_help;
 
 process_commandline_arguments() {
-	local -n flag_uninstall_ref="${1}"
+    local -n flag_uninstall_ref="${1}"
 
-	if [ "${#RUNTIME_COMMANDLINE_ARGUMENTS[@]}" -eq 0 ]; then
-		return 0
-	fi
+    if [ "${#RUNTIME_COMMANDLINE_ARGUMENTS[@]}" -eq 0 ]; then
+        return 0
+    fi
 
-	# modifyable parameters for parsing by consuming
-	local -a parameters=("${RUNTIME_COMMANDLINE_ARGUMENTS[@]}")
+    # modifyable parameters for parsing by consuming
+    local -a parameters=("${RUNTIME_COMMANDLINE_ARGUMENTS[@]}")
 
-	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
-	local enable_debug=N
+    # Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
+    local enable_debug=N
 
-	while true; do
-		if [ "${#parameters[@]}" -eq 0 ]; then
-			break
-		else
-			case "${parameters[0]}" in
-				--help\
-				|-h)
-					print_help;
-					exit 0
-					;;
-				--uninstall\
-				|-u)
-					flag_uninstall_ref=true
-					;;
-				--debug\
-				|-d)
-					enable_debug=Y
-					;;
-				*)
-					printf 'ERROR: Unknown command-line argument "%s"\n' "${parameters[0]}" >&2
-					return 1
-					;;
-			esac
-			# shift array by 1 = unset 1st then repack
-			unset 'parameters[0]'
-			if [ "${#parameters[@]}" -ne 0 ]; then
-				parameters=("${parameters[@]}")
-			fi
-		fi
-	done
+    while true; do
+        if [ "${#parameters[@]}" -eq 0 ]; then
+            break
+        else
+            case "${parameters[0]}" in
+                --help\
+                |-h)
+                    print_help;
+                    exit 0
+                    ;;
+                --uninstall\
+                |-u)
+                    flag_uninstall_ref=true
+                    ;;
+                --debug\
+                |-d)
+                    enable_debug=Y
+                    ;;
+                *)
+                    printf 'ERROR: Unknown command-line argument "%s"\n' "${parameters[0]}" >&2
+                    return 1
+                    ;;
+            esac
+            # shift array by 1 = unset 1st then repack
+            unset 'parameters[0]'
+            if [ "${#parameters[@]}" -ne 0 ]; then
+                parameters=("${parameters[@]}")
+            fi
+        fi
+    done
 
-	if [ "${enable_debug}" = Y ]; then
-		trap 'trap_return "${FUNCNAME[0]}"' RETURN
-		set -o xtrace
-	fi
-	return 0
+    if [ "${enable_debug}" = Y ]; then
+        trap 'trap_return "${FUNCNAME[0]}"' RETURN
+        set -o xtrace
+    fi
+    return 0
 }; declare -fr process_commandline_arguments
 
 determine_install_directory(){
-	local -n install_directory_xdg_ref="${1}"; shift
+    local -n install_directory_xdg_ref="${1}"; shift
 
-	# For $XDG_TEMPLATES_DIR
-	if [ -f "${HOME}"/.config/user-dirs.dirs ];then
-		# external file, disable check
-		#shellcheck disable=SC1090
-		source "${HOME}"/.config/user-dirs.dirs
+    # For $XDG_TEMPLATES_DIR
+    if [ -f "${HOME}"/.config/user-dirs.dirs ];then
+        # external file, disable check
+        #shellcheck disable=SC1090
+        source "${HOME}"/.config/user-dirs.dirs
 
-		if [ -v XDG_TEMPLATES_DIR ]; then
-			install_directory_xdg_ref="${XDG_TEMPLATES_DIR}"
-			return 0
-		fi
-	fi
+        if [ -v XDG_TEMPLATES_DIR ]; then
+            install_directory_xdg_ref="${XDG_TEMPLATES_DIR}"
+            return 0
+        fi
+    fi
 
-	printf -- \
-		"%s: Warning: Installer can't locate user-dirs configuration, will fallback to unlocalized directories\\n" \
-		"${FUNCNAME[0]}" \
-		1>&2
+    printf -- \
+        "%s: Warning: Installer can't locate user-dirs configuration, will fallback to unlocalized directories\\n" \
+        "${FUNCNAME[0]}" \
+        1>&2
 
-	if [ ! -d "${HOME}"/Templates ]; then
-		return 1
-	else
-		install_directory_xdg_ref="${HOME}"/Templates
-	fi
+    if [ ! -d "${HOME}"/Templates ]; then
+        return 1
+    else
+        install_directory_xdg_ref="${HOME}"/Templates
+    fi
 
 }; declare -fr determine_install_directory
 
 ## Attempt to remove old installation files
 remove_old_installation(){
-	local install_directory_xdg="${1}"; shift 1
+    local install_directory_xdg="${1}"; shift 1
 
-	printf 'Removing previously installed templates(if available)...\n'
-	rm\
-		--verbose\
-		--force\
-		"${install_directory_xdg}/EditorConfig Template.editorconfig"
-	rm\
-		--verbose\
-		--force\
-		"${HOME}/.local/share/templates/EditorConfig Template.editorconfig"\
-		"${HOME}/.local/share/templates/EditorConfig Template.desktop"
-	printf 'Finished.\n'
+    printf 'Removing previously installed templates(if available)...\n'
+    rm\
+        --verbose\
+        --force\
+        "${install_directory_xdg}/EditorConfig Template.editorconfig"
+    rm\
+        --verbose\
+        --force\
+        "${HOME}/.local/share/templates/EditorConfig Template.editorconfig"\
+        "${HOME}/.local/share/templates/EditorConfig Template.desktop"
+    printf 'Finished.\n'
 
-	printf '\n' # Additional blank line for separating output
-	return 0
+    printf '\n' # Additional blank line for separating output
+    return 0
 }; declare -fr remove_old_installation
 
 ## Traps: Functions that are triggered when certain condition occurred
 ## Shell Builtin Commands » Bourne Shell Builtins » trap
 trap_errexit(){
-	printf 'An error occurred and the script is prematurely aborted\n' 1>&2
-	return 0
+    printf 'An error occurred and the script is prematurely aborted\n' 1>&2
+    return 0
 }; declare -fr trap_errexit; trap trap_errexit ERR
 
 trap_exit(){
-	return 0
+    return 0
 }; declare -fr trap_exit; trap trap_exit EXIT
 
 trap_return(){
-	local returning_function="${1}"
+    local returning_function="${1}"
 
-	printf 'DEBUG: %s: returning from %s\n' "${FUNCNAME[0]}" "${returning_function}" 1>&2
+    printf 'DEBUG: %s: returning from %s\n' "${FUNCNAME[0]}" "${returning_function}" 1>&2
 }; declare -fr trap_return
 
 trap_interrupt(){
-	printf '\n' # Separate previous output
-	printf 'Recieved SIGINT, script is interrupted.' 1>&2
-	return 1
+    printf '\n' # Separate previous output
+    printf 'Recieved SIGINT, script is interrupted.' 1>&2
+    return 1
 }; declare -fr trap_interrupt; trap trap_interrupt INT
 
 init "${@}"
